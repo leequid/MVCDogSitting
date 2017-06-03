9c34ad6ca3ff23c6d3312d56c9133664c3863751
Ryan Grillo <ryangrillo@gmail.com>
2017-06-03 12:27:39 -0600

    started mapping

--- JPADogSitting/src/entities/User.java
+++ JPADogSitting/src/entities/User.java
@@ -5,13 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class User {
 	
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
+	@OneToOne
+	@JoinColumn(name="contact_id")
+	private Contact contact;   			//maps one to one with contact table
 	
 	@Column(name="user_name")
 	private String userName;
