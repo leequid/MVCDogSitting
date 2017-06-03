9697e336f510923ad29a6b35cdb1d99e08573971
Eric Lee <ericlee@Daewoos-Mac.local>
2017-06-03 10:03:01 -0600

    added db schema

---
+++ dsdb.sql
@@ -0,0 +1,127 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb` ;
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `sitter` TINYINT(1) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dog`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dog` ;
+
+CREATE TABLE IF NOT EXISTS `dog` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `dog_user_id` INT NOT NULL,
+  `weight` INT NULL,
+  `img_url` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_dog_user_idx` (`dog_user_id` ASC),
+  CONSTRAINT `dog_user_id`
+    FOREIGN KEY (`dog_user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sitter`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sitter` ;
+
+CREATE TABLE IF NOT EXISTS `sitter` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `size_pref` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `appointment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `appointment` ;
+
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `sitter_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `user_id_idx` (`user_id` ASC),
+  INDEX `sitter_id_idx` (`sitter_id` ASC),
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `sitter_id`
+    FOREIGN KEY (`sitter_id`)
+    REFERENCES `sitter` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dog_appointment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dog_appointment` ;
+
+CREATE TABLE IF NOT EXISTS `dog_appointment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `dog_id` INT NOT NULL,
+  `appointment_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `appointment_id_idx` (`appointment_id` ASC),
+  INDEX `dog_id_idx` (`dog_id` ASC),
+  CONSTRAINT `dog_id`
+    FOREIGN KEY (`dog_id`)
+    REFERENCES `dog` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `appointment_id`
+    FOREIGN KEY (`appointment_id`)
+    REFERENCES `appointment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+GRANT USAGE ON *.* TO admin;
+ DROP USER admin;
+SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE USER 'admin' IDENTIFIED BY 'admin';
+
+GRANT ALL ON * TO 'admin';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
