1c3e8a571ecfcd3b1977e3992b07a11a0bbb86d9
Ryan Grillo <ryangrillo@gmail.com>
2017-06-05 14:21:17 -0600

    added Junit testing to entities

--- JPADogSitting/test/test/SitterTest.java
+++ JPADogSitting/test/test/SitterTest.java
@@ -18,33 +18,40 @@ import entities.User;
 
 public class SitterTest {
 	private EntityManager em = null;
-    
-	
-	
-	@Before
-    public void setup() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("DogSitting");
-        em = emf.createEntityManager();
-    }
-    @After
-    public void tearDown() {
-        if (em != null) {
-            em.close();
-        }
-    }
-    @Test
-    public void test() {
-        boolean pass = true;
-        assertEquals(pass, true);
-    }
-    @Test
-    public void check_if_sitter_has_appointment() {
-   	 assertNotNull(em.find(Sitter.class, 1).getAppointments());
-    }
-    @Test
-    public void check_if_sitter_has_appointment_id_accessible() {
-    	System.out.println(em.find(Sitter.class, 1).getSizePreference());
-    	assertEquals(em.find(Sitter.class, 1).getAppointments().get(0).getStartDate().toString(), "2016-02-12");
-    }
-}
 
+	@Before
+	public void setup() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("DogSitting");
+		em = emf.createEntityManager();
+	}
+
+	@After
+	public void tearDown() {
+		if (em != null) {
+			em.close();
+		}
+	}
+
+	@Test
+	public void test() {
+		boolean pass = true;
+		assertEquals(pass, true);
+	}
+
+	@Test
+	public void check_if_sitter_has_appointment() {
+		assertNotNull(em.find(Sitter.class, 1).getAppointments());
+	}
+
+	@Test
+	public void check_if_sitter_has_appointment_id_accessible() {
+		System.out.println(em.find(Sitter.class, 1).getSizePreference());
+		assertEquals(em.find(Sitter.class, 1).getAppointments().get(0).getStartDate().toString(), "2016-02-12");
+	}
+
+	@Test
+	public void check_if_sitter_has_user_mapped_correctly() {
+		String expectedUserName = "Ryan";
+		assertEquals(em.find(Sitter.class, 2).getUser().getUserName(), expectedUserName);
+	}
+}
